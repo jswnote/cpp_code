d48ff36f41cdac5f57864d07e23ffc7c1b83ac94
swjung <121101975+jswnote@users.noreply.github.com>
2025-12-25 01:31:02 +0900

    Update sort.cpp

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -134,24 +134,3 @@ void count_sort(vector<int>& arr)
         arr[m] = new_arr[m];
     }
 }
-
-template <typename T>
-int binary_search(vector<T>& arr, int start, int end, int target)
-{
-    int mid = (start + end) / 2;
-    int target_idx;
-
-    if(arr[mid] < target)
-    {
-        binary_search(arr, start, mid, target);
-    }
-
-    else if (arr[mid] > target)
-    {
-        binary_search(arr, mid+1, end, target);
-    }
-    else if (arr[mid] == target)
-    {
-        return mid;
-    }
-}

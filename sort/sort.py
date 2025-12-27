62f2d30a8d5ccce3032c3dc24427ca6bedaa97d1
swjung <121101975+jswnote@users.noreply.github.com>
2025-12-28 02:12:15 +0900

    Update sort.py

--- sort/sort.py
+++ sort/sort.py
@@ -11,3 +11,11 @@ def count_sort(arr):
         if count[j]:
             for _ in range(count[j]):
                 print(j)
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        minimum = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[minimum]:
+                minimum = j
+        arr[i], arr[minimum] = arr[minimum], arr[i]

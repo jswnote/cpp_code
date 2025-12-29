0170e11e48bd2f79c079e36d1f2b0fa4ec588656
swjung <121101975+jswnote@users.noreply.github.com>
2025-12-29 15:39:40 +0900

    Create bj_11047.py

---
+++ greedy/bj_11047.py
@@ -0,0 +1,17 @@
+N, K = map(int,input().split())
+
+arr = []
+
+for i in range(N):
+    a = int(input())
+    arr.append(a)
+    
+arr.reverse()
+    
+sum = 0
+for j in range(len(arr)):
+    v = K // arr[j]
+    K = K % arr[j]
+    sum += v
+    
+print(sum)

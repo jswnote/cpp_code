484ed6e1e275b6a92fe8ed9a42bfccdfc9172599
swjung <121101975+jswnote@users.noreply.github.com>
2026-01-05 02:28:51 +0900

    Create DFS_BFS.py

---
+++ DFS_BFS/DFS_BFS.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def DFS(graph, v, visited):
+    visited = True
+    print(v, end = " ")
+
+    for i in graph[v]:
+        if not visited[i]:
+            DFS(graph, i, visited)
+
+def BFS(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end = " ")
+
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
